a4906f0ef5a4e2e5b21741b03cd5b3b9e5cc0414
unknown <jtjackson325@gmail.com>
2021-09-12 14:12:48 -0600

    got the service worker registered

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,21 @@
+const APP_PREFIX = 'OfflineFunctional-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CATCHE = [
-    ""
-]
+    "./models/transaction.js",
+    "./public/css/style.css",
+    "./public/js/idb.js",
+    "./public/js/index.js",
+    "./public/index.html",
+    "./routes.api.js"
+];
 
 self.addEventListener('install', function (e) {
-
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache: ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CATCHE)
+        })
+    )
 })
