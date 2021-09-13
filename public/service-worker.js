a8b97dde2b42caa47020763cab9319e7fe6881b2
unknown <jtjackson325@gmail.com>
2021-09-12 19:14:15 -0600

    got the first part of the service worker working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,19 @@
-const APP_PREFIX = 'OfflineFunctional-';
+const APP_PREFIX = 'BudgetTrack-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-const FILES_TO_CATCHE = [
-    "./models/transaction.js",
-    "./public/css/style.css",
-    "./public/js/idb.js",
-    "./public/js/index.js",
-    "./public/index.html",
-    "./routes.api.js"
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/index.js",
+    "./js/idb.js"
 ];
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache: ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CATCHE)
+            return cache.addAll(FILES_TO_CACHE)
         })
     )
 })
